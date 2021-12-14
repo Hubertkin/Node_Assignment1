cb9f1b5fd451709847633579ad6631f42b45e79a
Hubert Kingsley Ocran <kingsleyocran94@gmail.com>
2021-12-14 13:21:38 +0000

    node assignment

--- express_server.js
+++ express_server.js
@@ -1,29 +1,56 @@
-require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const userController = require('./Controller/userController');
 const PORT = process.env.PORT || 4000
 
 
 app.use(express.json());
 
-app.get('/',function(req,res){
-  res.status(200).json({message: 'Welcome to Hubert\'s User api'})
+app.get('',function getUsers (req,res){
+  try{
+  res.status(200).json({
+    "Users": [
+        {
+            "first_name": "Hubert ",
+            "last_name": "Ocran",
+            "date_of_birth": "2005-04-10T23:00:00.000Z",
+            "school": "Bosome Senior High Tech School",
+        },
+        {
+            "first_name": "Gabriel ",
+            "last_name": "Owusu-Ansah",
+            "date_of_birth": "1995-09-08T23:00:00.000Z",
+            "school": "Kumasi Technical University",
+        },
+        {
+            "first_name": "Joseph ",
+            "last_name": "kwakye",
+            "date_of_birth": "1999-08-09T23:00:00.000Z",
+            "school": "Kwame Nkrumah University Of Science And Technology"
+        }
+    ]
+
+})}catch (error){
+  console.log('Can\'t get users : ',error.message);     
+  res.status(404).json({message :'Can\'t get users ', error:error.message})
+}
 });
 
-app.get('/users', userController.getAllUsers);
+app.post('',function addUser (req,res){
+  try{
+  res.status(200).json({message: 'User has been created'})
+}catch(error){
+  console.log('Can\'t add user: ',error.message);     
+  res.status(404).json({message :'Can\'t add user ', error:error.message})
+}
+})
 
-app.post('/user', userController.addUser);
+
+
+
 
 app.listen(PORT, function () {
     console.log('Server has started to run');
-    mongoose.connect(process.env.DB_URL)
-    .then(function(){
-      console.log('DB is connected')
-    })
-    .catch(function(error){
-      console.log('DB is not connected :',error.message);
-    })
+
   
   })
